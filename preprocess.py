2c708d93b41b82d0aea7f3ff01543dcba3e124f0
Rylan Marianchuk <45247316+rylan-marianchuk@users.noreply.github.com>
2021-04-08 19:26:35 -0600

    Load with resizing

--- preprocess.py
+++ preprocess.py
@@ -1,155 +1,23 @@
 import pandas as pd
 import imgaug as ia
 import os
-#import imageio
+import imageio
 import numpy as np
-from sklearn.manifold import Isomap
 from PIL import Image
-from PIL.ImageOps import grayscale
+from sklearn.manifold import Isomap
 from sklearn.model_selection import train_test_split
 
-cream_bg = (195, 201, 198)
 
-# Euclidean distance between two vectors
-def dist(vec1, vec2):
-    return np.sqrt(sum((vec1[i]-vec2[i])**2 for i in range(len(vec1))))
-
-def mask(img):
-    # If all channels are below some threshold, turn to cream
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            # Mask all but blue
-            if dist((0, 0, 0), img[i][j]) < 45:
-              img[i][j][0] = cream_bg[0]
-              img[i][j][1] = cream_bg[1]
-              img[i][j][2] = cream_bg[2]
-
-    #ia.imshow(img)
-
-def augment():
-    """
-    Need only be run once
-
-    Augmentations to apply:
-
-    :return: Create a new directory with augmented imaged using imgaug
-    """
-
-    # Reading csv for label
-    df = pd.read_csv(r"dataset-master/dataset-master/labels.csv")
-    label_col = df["Category"].tolist()
-    labels = []
-    inDIR = r"dataset-master/dataset-master/JPEGImages"
-    count = 0
-    images = []
-    for im in os.listdir(inDIR):
-        if type(label_col[count]) == type(float()) or len(label_col[count].split(" ")) != 1:
-            # 0 or more than one label, discard
-            count += 1
-            continue
-
-        imagePIL = Image.open(inDIR + "/" + im)
-        image_as_array = np.array(imagePIL)
-        for _ in range(28):
-            images.append(image_as_array)
-            labels.append(label_col[count])
-        count += 1
-    print("COUNT (number of original images duplicated 28 times: " + str(count))
-    print("TOTAL images to create: " + str(count * 28))
-
-    flipH = ia.augmenters.Fliplr(0.3333)
-    flipV = ia.augmenters.Flipud(0.3333)
-    one = ia.augmenters.Sometimes(0.25, ia.augmenters.Affine(
-            scale={"x": (0.8, 1.12), "y": (0.8, 1.12)}))
-    two = ia.augmenters.Sometimes(0.25, ia.augmenters.Affine(
-            translate_percent={"x": (-0.15, 0.15), "y": (-0.15, 0.15)}, shear=(-3, 3)))
-    three = ia.augmenters.Sometimes(0.55, ia.augmenters.Affine(
-            rotate=(-45, 45)))
-    augmented = flipH(images=images)
-    augmented = flipV(images=augmented)
-    augmented = one(images=augmented)
-    augmented = two(images=augmented)
-    augmented = three(images=augmented)
-
-    for img, i in zip(augmented, range(len(augmented))):
-        mask(img)
-        #ia.imshow(img)
-        save = Image.fromarray(img.astype('uint8'), 'RGB')
-        save.save(r"augmented/" + labels[i] + str(i) + ".png", "PNG")
-        #imageio.imwrite(r"augmented/" + labels[i] + str(i), img)
-
-    # Write labels to csv
-    pd.DataFrame({'label': labels}).to_csv("auglabels.csv")
-
-def load_batch_images(batch, useGrayscale=False, reduce=False, flatten=False):
-    """
-        Load a batch of image data, with optional processing.
-        
-        batch - array of image filenames located in the 'augmented' directory
-        useGrayscale - if True, converts the image to grayscale
-        reduce - if True, processes the image with isomap reduction
-        flatten - if True, flattens the image matrices
-        
-        Returns a Numpy array of image matrices of shape (480, 640, 3) or (480, 640, 1) for grayscale,
-        with optional flattening.
-        
-    """
-    loadedBatch = []
-    print("Loading image batch...")
-
-    # Load images from this batch
-    for im in batch:
-        im = Image.open(r"augmented/" + im)
-            
-        # Additional processing
-        if useGrayscale:
-            im = grayscale(im)
-            
-        # Convert image to Numpy array
-        im = np.asarray(im)
-        
-        # Flatten the image
-        if flatten:
-            im = im.flatten()
-            
-        # Store image and label 
-        loadedBatch.append(im)
-    
-    # Apply isomap dimension reduction
-    if reduce:
-        loadedBatch = Data.isomap_reduce(loadedBatch, 50)
-
-    return loadedBatch
-
-def load_reduced_batches(batches):
-    """
-    Load
-
-    Parameters
-    ----------
-    batches : generator
-        Yields batches of image filenames to load.
-        
-    Returns
-    -------
-    reduced_images : Numpy array
-        array of processed image vectors
-
-    """
-    
-    reduced_images = []
-
-    # Reduce all images in each batch
-    for b in batches:
-        reduced_images.extend(load_batch_images(b, useGrayscale=True, reduce=True, flatten=True))
-            
-    return np.array(reduced_images)
 
 class Data:
-    
+
+    """
+    Load the image data and augment to 10k observations on the fly
+    """
+
     # Maps each class of white blood cell to an integer label
-    class_map = {0 : "EOSINOPHIL", 1 : "LYMPHOCYTE", 2 : "MONOCYTE", 3 : "NEUTROPHIL", 4 : "BASOPHIL",
-                 "EOSINOPHIL" : 0, "LYMPHOCYTE" : 1, "MONOCYTE" : 2, "NEUTROPHIL" : 3, "BASOPHIL" : 4 }
+    class_map = {0 : "EOSINOPHIL", 1 : "LYMPHOCYTE", 2 : "MONOCYTE", 3 : "NEUTROPHIL",
+                 "EOSINOPHIL" : 0, "LYMPHOCYTE" : 1, "MONOCYTE" : 2, "NEUTROPHIL" : 3 }
     
     # The image data. Each entry is the filename for a 480x640 image.
     # Converted to a numpy array after the constructor is called
@@ -159,34 +27,39 @@ class Data:
     # Converted to a numpy array after the constructor is called
     Y = []
     
-    #### Counting distribution of each class
-    #distribution = {"EOSINOPHIL" : 0, "LYMPHOCYTE" : 0, "MONOCYTE" : 0, "NEUTROPHIL" : 0, "BASOPHIL" : 0}
-
-    def __init__(self):
+    normalized = False
+    def __init__(self, scale, normalize=False, read_from_disk=False):
         """
         Read in the augmented data which is ready for models
-        """
-        
-        augDir = r"augmented"
-        csv_labels = pd.read_csv("auglabels.csv")["label"].tolist()
-        i = 0
-        
-        # Loop over image files, ensuring order of filenames matches labels CSV
-        # https://stackoverflow.com/questions/33159106/sort-filenames-in-directory-in-ascending-order
-        for im in sorted(os.listdir(augDir), key=lambda f: int(''.join(filter(str.isdigit, f)))):
-            # Store image filenames
-            self.X.append(im)
-            
-            # Get the integer mapping of this image label
-            label_as_int = self.class_map[csv_labels[i]]
-            self.Y.append(label_as_int)
-            
-            #### For counting distribution of blood cell types
-            #self.distribution[csv_labels[i]] += 1
-            i += 1
 
-        self.X = np.array(self.X)
-        self.Y = np.array(self.Y)
+        :param scale float how much to resize width and height before augmenting
+        :param normalize: if True, values of the image array are floats from 0 to 1, otherwise uint8 between 0, 255
+        """
+        self.normalized = normalize
+
+        if read_from_disk:
+            augDir = r"augmented"
+            csv_labels = pd.read_csv("auglabels.csv")["label"].tolist()
+            i = 0
+
+            # Loop over image files, ensuring order of filenames matches labels CSV
+            # https://stackoverflow.com/questions/33159106/sort-filenames-in-directory-in-ascending-order
+            for im in sorted(os.listdir(augDir), key=lambda f: int(''.join(filter(str.isdigit, f)))):
+                # Store image filenames
+                self.X.append(im)
+
+                # Get the integer mapping of this image label
+                label_as_int = self.class_map[csv_labels[i]]
+                self.Y.append(label_as_int)
+
+                #### For counting distribution of blood cell types
+                #self.distribution[csv_labels[i]] += 1
+                i += 1
+
+        else:
+            self.augment(scale)
+            self.X = np.array(self.X)
+            self.Y = np.array([self.class_map[s] for s in self.Y])
         
         return
     
@@ -211,8 +84,9 @@ class Data:
         self.X, self.Y, stratify=self.Y, test_size=0.2, random_state=random_state)
         
         return (X_train, X_test, y_train, y_test)
-    
-    def isomap_reduce(batch, lower_dimensions, consider_n_neighbours=10):
+
+
+    def isomap_reduce(self, batch, lower_dimensions, consider_n_neighbours=10):
         """
         Apply the isomap dimension reduction on the batch
         :param batch: a subset of the class data self.X. Each entry is an image of 3 RGB channels
@@ -220,3 +94,115 @@ class Data:
         """
         isomap = Isomap(n_components=lower_dimensions, n_neighbors=consider_n_neighbours)
         return isomap.fit_transform(batch)
+
+    cream_bg = (195, 201, 198)
+
+    # Euclidean distance between two vectors
+    def dist(self, vec1, vec2):
+        return np.sqrt(sum((vec1[i] - vec2[i]) ** 2 for i in range(len(vec1))))
+
+    def mask(self, img):
+        # If all channels are below some threshold, turn to cream
+        for i in range(img.shape[0]):
+            for j in range(img.shape[1]):
+                # Mask all but blue
+                if self.dist((0, 0, 0), img[i][j]) < 45:
+                    img[i][j][0] = self.cream_bg[0]
+                    img[i][j][1] = self.cream_bg[1]
+                    img[i][j][2] = self.cream_bg[2]
+        return
+
+
+    def augment(self, scale, save_to_disk=False):
+        """
+        Run on each instance of this class
+
+        Augmentations to apply:
+        :param scale float how much to resize width and height before augmenting
+        :param save_to_disk: if True, all augmented images are saved, otherwise just loaded into self.X
+        :return: augmented dataset
+        """
+
+
+        # How many duplications of non augmented needed
+        dups = 13
+
+        # Reading csv for label
+        df = pd.read_csv(r"dataset-master/dataset-master/labels.csv")
+        label_col = df["Category"].tolist()
+        labels = []
+        inDIR = r"dataset-master/dataset-master/JPEGImages"
+        count = 0
+        using = 0
+        images = []
+        for im in os.listdir(inDIR):
+            if type(label_col[count]) == type(float()) or len(label_col[count].split(" ")) != 1 or label_col[count] == 'BASOPHIL' :
+                # 0 or more than one label, discard
+                count += 1
+                continue
+
+            imagePIL = Image.open(inDIR + "/" + im)
+            imagePIL = imagePIL.resize((int(imagePIL.size[0]*scale), int(imagePIL.size[1]*scale)) , Image.BICUBIC)
+
+
+            if self.normalized:
+                # Center crop
+                width, height = imagePIL.size  # Get dimensions
+                new_width, new_height = (265, 265)
+                left = (width - new_width) / 2
+                top = (height - new_height) / 2
+                right = (width + new_width) / 2
+                bottom = (height + new_height) / 2
+
+                # Crop the center of the image
+                imagePIL = imagePIL.crop((left, top, right, bottom))
+
+                # Convert to float 16 to normalize here
+                image_as_array = np.array(imagePIL).astype(np.float32) / 255
+            else: image_as_array = np.array(imagePIL)
+            for _ in range(dups):
+                images.append(image_as_array)
+                labels.append(label_col[count])
+            count += 1
+            using += 1
+        print("New size:")
+        print(images[0].shape)
+        print("Number of images dropped: " + str(count - using))
+        print("COUNT (number of original images duplicated " + str(dups) + " times: " + str(using))
+        print("TOTAL images to create: " + str(using * dups))
+
+
+        one = ia.augmenters.Sometimes(0.35, ia.augmenters.Affine(
+            scale={"x": (0.8, 1.12), "y": (0.8, 1.12)}))
+        two = ia.augmenters.Sometimes(0.35, ia.augmenters.Affine(
+            translate_percent={"x": (-0.15, 0.15), "y": (-0.15, 0.15)}, shear=(-3, 3)))
+        three = ia.augmenters.Sometimes(0.55, ia.augmenters.Affine(
+            rotate=(-45, 45)))
+
+        aug = ia.augmenters.SomeOf((1, None), [
+            ia.augmenters.Affine(rotate=(-45, 45)),
+            ia.augmenters.Affine(
+                translate_percent={"x": (-0.15, 0.15), "y": (-0.15, 0.15)}, shear=(-3, 3)),
+            ia.augmenters.Affine(
+                scale={"x": (0.8, 1.12), "y": (0.8, 1.12)})
+        ], random_order=True)
+
+        flipH = ia.augmenters.Fliplr(0.3333)
+        flipV = ia.augmenters.Flipud(0.3333)
+        augmented = flipH(images=images)
+        augmented = flipV(images=augmented)
+        augmented = aug(images=augmented)
+
+        if save_to_disk:
+            for img, i in zip(augmented, range(len(augmented))):
+                self.mask(img)
+                # ia.imshow(img)
+                save = Image.fromarray(img.astype('uint8'), 'RGB')
+                save.save(r"augmented/" + labels[i] + str(i) + ".png", "PNG")
+                # imageio.imwrite(r"augmented/" + labels[i] + str(i), img)
+
+            # Write labels to csv
+            pd.DataFrame({'label': labels}).to_csv("auglabels.csv")
+
+        self.X = augmented
+        self.Y = labels
