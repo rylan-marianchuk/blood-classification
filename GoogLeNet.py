86d297e53a601ddeb6c8d2fcce9de09e39bc8387
Rylan Marianchuk <45247316+rylan-marianchuk@users.noreply.github.com>
2021-04-08 19:26:58 -0600

    Working

--- GoogLeNet.py
+++ GoogLeNet.py
@@ -8,12 +8,32 @@ GoogLeNet weights are frozen, added a k neuron layer that is fully connected at 
 
 
 import torch
-from torch.utils.data import DataLoader, TensorDataset, random_split
+from torch.utils.data import DataLoader, TensorDataset, random_split, Dataset
 import numpy as np
 from torchvision.models import googlenet
 from preprocess import Data
+import torchvision.transforms as transforms
 
+class CustomTensorDataset(Dataset):
+    """TensorDataset with support of transforms.
+    """
+    def __init__(self, tensors, transform=None):
+        assert all(tensors[0].size(0) == tensor.size(0) for tensor in tensors)
+        self.tensors = tensors
+        self.transform = transform
 
+    def __getitem__(self, index):
+        x = self.tensors[0][index]
+
+        if self.transform:
+            x = self.transform(x)
+
+        y = self.tensors[1][index]
+
+        return x, y
+
+    def __len__(self):
+        return self.tensors[0].size(0)
 
 # Classes
 k = 4
@@ -27,21 +47,24 @@ model.requires_grad_(False)
 model.fc = torch.nn.Linear(model.fc.in_features, k)
 
 # Grab data and turn into dataset
-data = Data()
+data = Data(scale=0.55, normalize=True)
 int_labels = torch.tensor(data.Y)
-all = TensorDataset(torch.tensor(data.X), int_labels)
-
+#all = CustomTensorDataset(tensors=(torch.tensor(data.X), int_labels), transform=
+#            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]))
+X = torch.tensor(data.X)
+X_perm = X.permute(0, 3, 1, 2)
+all = TensorDataset(X_perm, int_labels)
 # Obs
 n = len(all)
-train_size = int(n*0.7)
-test_size = n - int(n*0.7)
+train_size = int(n*0.8)
+test_size = n - int(n*0.8)
 train, test = random_split(all, lengths=[train_size, test_size])
 
 # Main train block
 batch_size = 128
 params = {
     'batch_size': batch_size,
-    'shuffle': True,
+    'shuffle': True
 }
 loader_tr = DataLoader(train, **params)
 loader_te = DataLoader(test, **params)
@@ -64,6 +87,7 @@ for epoch in range(30):
 
     for batch_idx, batch in enumerate(loader_tr):
         X, y = batch
+        y = y.type(torch.LongTensor)
         X = X.to("cuda")
         y = y.to("cuda")
         optimizer.zero_grad()
